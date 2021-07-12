b4e697fd4b969c2897d3741a04e65b94238e55f9
KhaosWulf <65934392+KhaosWulf@users.noreply.github.com>
2021-07-12 00:30:24 -0400

    Deleted Homepage.js
    Was unnecessary.

--- src/main/webapp/Homepage.js
+++
@@ -1,9 +0,0 @@
-submitAllForms = function(){
-    document.getElementById("guidelines").submit();
-    document.getElementById("description").submit();
-    document.getElementById("name").submit();
-    document.getElementById("address").submit();
-    document.getElementById("number").submit();
-    document.getElementById("dates").submit();
-    document.getElementById("opportunities").submit();
-}
