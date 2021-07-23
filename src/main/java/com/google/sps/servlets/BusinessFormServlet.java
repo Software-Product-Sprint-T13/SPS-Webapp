6e75450a8be0f8aadf0df9a6086ad14df7586eab
Kizito Akwotiga <kakwotiga@yahoo.com>
2021-07-23 01:09:55 +0000

    update servlets

--- src/main/java/com/google/sps/servlets/BusinessFormServlet.java
+++ src/main/java/com/google/sps/servlets/BusinessFormServlet.java
@@ -23,53 +23,13 @@ public class BusinessFormServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    /*
-    String description = request.getParameter("description-input");
-    String name = request.getParameter("name-input");
-    String address = request.getParameter("address-input");
-    String city = request.getParameter("city-input");
-    String state = request.getParameter("state-input");
-    String zip = request.getParameter("zip-input");
-    String number = request.getParameter("number-input");
-
-
-    // // Print the input so you can see it in the server logs.
-    System.out.println("description: " + description);
-    System.out.println("name: " + name);
-    System.out.println("address: " + address);
-    System.out.println("City: " + city);
-    System.out.println("State: " + state);
-    System.out.println("Zip: " + zip);
-    System.out.println("Number: " + number);
-    
-
-    // Write the input to the response so the user can see it.
-    response.setContentType("text/html;");
-    response.getWriter().println("<p>Description: " + description + "</p>");
-    response.getWriter().println("<p>Name: " + name + "</p>");
-    response.getWriter().println("<p>Address: " + address + "</p>");
-    response.getWriter().println("<p>City: " + city + "</p>");
-    response.getWriter().println("<p>State: " + state + "</p>");
-    response.getWriter().println("<p>Zip: " + zip + "</p>");
-    response.getWriter().println("<p>Contact Number: " + number + "</p>");
-  */
-
-
-
-
-
-
-
-
-
-    //response.getWriter().println("<p>Number: " + number + "</p>");
-    // Sanitize user input to remove HTML tags and JavaScript.
+   // Sanitize user input to remove HTML tags and JavaScript.
     //look for the name of the element for the input in the html file
 
     
     String description = Jsoup.clean(request.getParameter("description-input"), Whitelist.none());
     String name = Jsoup.clean(request.getParameter("name-input"), Whitelist.none());
-    String address = Jsoup.clean(request.getParameter("address-input"), Whitelist.none());
+    String street = Jsoup.clean(request.getParameter("address-input"), Whitelist.none());
     String city = Jsoup.clean(request.getParameter("city-input"), Whitelist.none());
     String state = Jsoup.clean(request.getParameter("state-input"), Whitelist.none());
     String zip = Jsoup.clean(request.getParameter("zip-input"), Whitelist.none());
@@ -89,6 +49,11 @@ public class BusinessFormServlet extends HttpServlet {
     String sat2 = Jsoup.clean(request.getParameter("sat2"), Whitelist.none());
     String tue1 = Jsoup.clean(request.getParameter("tue1"), Whitelist.none());
     String tue2 = Jsoup.clean(request.getParameter("tue2"), Whitelist.none());
+
+    // String email = Jsoup.clean(request.getParameter("email-input"), Whitelist.none());
+    // String website = Jsoup.clean(request.getParameter("website-input"), Whitelist.none());
+
+
     
 
 
@@ -101,7 +66,10 @@ public class BusinessFormServlet extends HttpServlet {
         Entity.newBuilder(keyFactory.newKey())
             .set("description", description)
             .set("name", name)
-            .set("street", address)
+            .set("timestamp", timestamp)
+            // .set("email", email)
+            // .set("website", website)
+            .set("street", street)
             .set("city", city)
             .set("state", state)
             .set("zipcode", zip)
@@ -121,12 +89,10 @@ public class BusinessFormServlet extends HttpServlet {
             .set("friday2", fri2)
             .set("saturday1", sat1)
             .set("saturday2", sat2)
-            .set("timestamp", timestamp)
+            
             .build();
     datastore.put(taskEntity);
     
-    System.out.println("name: " + name + address + city + state + zip + number + description + apprenticeship + sun1 + sun2);
-
     response.sendRedirect("/BusinessForm.html");
   }
 }
--- src/main/java/com/google/sps/servlets/ShowBusinessFormServlet.java
+++ src/main/java/com/google/sps/servlets/ShowBusinessFormServlet.java
@@ -1,5 +1,5 @@
 
-package com.google.sps.servlets;
+package com.google.sps.servlets; 
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -23,28 +23,86 @@ import com.google.gson.Gson;
 @WebServlet("/show-form")
 public class ShowBusinessFormServlet extends HttpServlet {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-    Query<Entity> query =
-        Query.newEntityQueryBuilder().setKind("Business Data").setOrderBy(OrderBy.desc("name")).build();
-    QueryResults<Entity> results = datastore.run(query);
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+        Query<Entity> query = Query.newEntityQueryBuilder().setKind("Business Data").setOrderBy(OrderBy.desc("name"))
+                .build();
+        QueryResults<Entity> results = datastore.run(query);
 
-    List<Task> tasks = new ArrayList<>();
-    while (results.hasNext()) {
-      Entity entity = results.next();
+        List<Task> tasks = new ArrayList<>();
+        while (results.hasNext()) {
+            Entity entity = results.next();
+            // System.out.println(entity.getProperties().get("street").get());
+            long id = entity.getKey().getId();
+            String name = entity.getString("name");
+            long timestamp = entity.getLong("timestamp");
+            String street = entity.contains("street")?entity.getString("street"):"";
+            String city = entity.contains("city")?entity.getString("city"):"";
+            String state = entity.contains("state")?entity.getString("state"):"";
+            String zip = entity.contains("zipcode")?entity.getString("zipcode"):"";
+            String tel = entity.contains("Tel")?entity.getString("Tel"):"";
+            String description = entity.contains("description")?entity.getString("description"):"";
+            String apprenticeship = entity.contains("apprenticeship")?entity.getString("apprenticeship"):"";
 
-      long id = entity.getKey().getId();
-      String name = entity.getString("name");
-      long timestamp = entity.getLong("timestamp");
-     
-      Task task = new Task(id, name, timestamp);
-      tasks.add(task);
+            String email = entity.contains("email")?entity.getString("email"):"";
+            String website = entity.contains("website")?entity.getString("website"):"";
+
+            // availability
+            String sunFrom = entity.contains("sunday1") ? entity.getString("sunday1") : "";
+            String sunTo = entity.contains("sunday2") ? entity.getString("sunday2") : "";
+            String monFrom = entity.contains("monday1") ? entity.getString("monday1") : "";
+            String monTo = entity.contains("monday2") ? entity.getString("monday2") : "";
+            String tueFrom = entity.contains("tuesday1") ? entity.getString("tuesday1") : "";
+            String tueTo = entity.contains("tuesday2") ? entity.getString("tuesday2") : "";
+            String wedFrom = entity.contains("wednesday1") ? entity.getString("wednesday1") : "";
+            String wedTo = entity.contains("wednesday2") ? entity.getString("wednesday2") : "";
+            String thurFrom = entity.contains("thursday1") ? entity.getString("thursday1") : "";
+            String thurTo = entity.contains("thursday2") ? entity.getString("thursday2") : "";
+            String friFrom = entity.contains("friday1") ? entity.getString("friday1") : "";
+            String friTo = entity.contains("friday2") ? entity.getString("friday2") : "";
+            String satFrom = entity.contains("saturday1") ? entity.getString("saturday1") : "";
+            String satTo = entity.contains("saturday2") ? entity.getString("saturday2") : "";
+
+
+
+
+            // System.out.println("data query: ");
+
+            Task task = new Task(
+                id,
+                name, 
+                timestamp, 
+                street, 
+                city, 
+                state, 
+                zip, 
+                tel, 
+                description, 
+                apprenticeship, 
+                email, 
+                website, 
+                sunFrom,
+                sunTo,
+                monFrom,
+                monTo,
+                tueFrom,
+                tueTo,
+                wedFrom,
+                wedTo,
+                thurFrom,
+                thurTo,
+                friFrom,
+                friTo,
+                satFrom,
+                satTo
+                );
+            tasks.add(task);
+        }
+
+        Gson gson = new Gson();
+
+        response.setContentType("application/json;");
+        response.getWriter().println(gson.toJson(tasks));
     }
-
-    Gson gson = new Gson();
-
-    response.setContentType("application/json;");
-    response.getWriter().println(gson.toJson(tasks));
-  }
 }
